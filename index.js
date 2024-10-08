16ecba1132cd61d15d9841f1dfee451dc4a1e2c9
KetlynHerrmann <163474832+KetlynHerrmann@users.noreply.github.com>
2024-10-08 23:09:44 +0000

    08/10/24

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const prompt = require('prompt-sync');
 const entrada = prompt( );
 
+var red = '\u001b[31m';
+var azulClaro = '\u001b[34m';
+var reset = '\u001b[0m';
+
+
 const nomeCompleto = "Ketlyn Silvestre Herrmann";
 const anoNascimento = 2006;
 let anoAtual = 2024;
@@ -8,19 +13,26 @@ let idade = 18;
 
 console.log("Olá eu sou " + nomeCompleto + " estamos no ano " + anoAtual + " minha idade é " + idade + " e sou nascida no ano de " + anoNascimento + ".");
 
+console.log(" ");
+
 anoAtual = anoAtual + 1;
 idade = idade + 1;
 console.log("Olá meu nome ainda é " + nomeCompleto + "," + " estamos no ano de " + anoAtual + " e finalmente fiz " + idade + " anos, " + " como eu sou nascida em 2006 ano que vem faço 20 anos e as minhas responsabilidades aumentam.");
 
-console.log(`oi eu sou ${nomeCompleto} meu ano de nascimento é ${anoNascimento} meu ano atual é ${anoAtual} e minha idade é ${idade}`);
+console.log(" ");
+
+console.log(`Oi eu sou ${nomeCompleto} meu ano de nascimento é ${anoNascimento} meu ano atual é ${anoAtual} e minha idade é ${idade}`);
 
 const loginCerto = "Fulano1234";
 const senhaCerta = 1234;
 console.log(" ");
-console.log("✈︎ AGÊNCIA DE VIAGENS ✈︎");
+console.log(red+"                                     ✈︎ AGÊNCIA DE VIAGENS ✈︎ "+reset);
 console.log(" ");
 
-console.log("Faça seu login para prosseguir");
+console.log(azulClaro+"                                         ARÉA DE LOGIN"+reset);
+console.log(" ");
+console.log("Faça seu login para prosseguir !");
+console.log(" ");
 
 var login = entrada("Digite seu usuário: ");
 var senha = entrada("Digite sua senha: ");
@@ -39,6 +51,8 @@ const listaDeViagens = new Array(
 `Alemanha`,
 `Italia`,
 );
+
+console.log(" "),
 
 console.log(listaDeViagens);
 
